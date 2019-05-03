87ff1756620599084e7093ee9e9b17d691c60b02
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-02 20:06:22 -0500

    change

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,7 +12,10 @@ node {
 
       
 }
-    stage("Move Index"){
-        sh "ssh ec2-user@54.194.169.141  sudo mv/tmp/index.html  /var/www/html/index.html"
-}
+   stage("Move index"){
+        sh "ssh  ec2-user@@54.194.169.141     sudo mv /tmp/index.html        /var/www/html/index.html"
+    }
+    stage("Restart webserver"){
+        sh "ssh  ec2-user@@54.194.169.141      sudo systemctl restart httpd "
+    }
 }
