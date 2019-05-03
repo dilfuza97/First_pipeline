7f347f2a31d7ac0168eed43ad5ce6720b79ad8dc
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-02 19:55:07 -0500

    change

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,5 +2,8 @@ node {
      stage("Pull Repo"){
          git 'https://github.com/dilfuza97/Cool_website.git'
      }
+     stage("Websever Install"){
+         sh "ssh ec2-user@54.194.169.141  sudo yum install http -y "
+
  }
       
