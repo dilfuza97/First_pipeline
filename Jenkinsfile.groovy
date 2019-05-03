761be79cc9df4aead3c45a239f6f8482b3f8f672
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-02 20:32:50 -0500

    added varible

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,22 +1,23 @@
 node {
-    properties([parameters([string(defaultValue: 'IP', description: 'where should i build?', name: 'Environment', trim: false)]), pipelineTriggers([pollSCM('* * * * *')])])
+    properties([parameters([string(defaultValue: 'IP', description: 'Where should I build? ', name: 'Environment', trim: true)]), pipelineTriggers([pollSCM('* * * * *')])])
+
      stage("Pull Repo"){
          git 'https://github.com/dilfuza97/Cool_website.git'
      }
      stage("Websever Install"){
-         sh "ssh ec2-user@54.194.169.141  sudo yum install httpd -y "
+         sh "ssh ec2-user@${Environment}  sudo yum install httpd -y "
 
  }
      stage("Index file"){
-         sh "scp index.html ec2-user@54.194.169.141:/tmp"
+         sh "scp index.html ec2-user${Environment}:/tmp"
 
 
       
 }
    stage("Move index"){
-        sh "ssh  ec2-user@54.194.169.141     sudo mv /tmp/index.html        /var/www/html/index.html"
+        sh "ssh  ec2-user@${Environment}     sudo mv /tmp/index.html        /var/www/html/index.html"
     }
     stage("Restart webserver"){
-        sh "ssh  ec2-user@54.194.169.141      sudo systemctl restart httpd "
+        sh "ssh  ec2-user@${Environment}     sudo systemctl restart httpd "
     }
 }
