2956ca83893f7d9038efeb89daceac36266aac69
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-02 19:55:44 -0500

    ch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ node {
          git 'https://github.com/dilfuza97/Cool_website.git'
      }
      stage("Websever Install"){
-         sh "ssh ec2-user@54.194.169.141  sudo yum install http -y "
+         sh "ssh ec2-user@54.194.169.141  sudo yum install httpd -y "
 
  }
       
